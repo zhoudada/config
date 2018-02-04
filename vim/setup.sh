65273c20a6d081c855b95e8787523a3cd1e513fc
zhoudada <zhoudada123@hotmail.com>
2018-02-04 12:18:05 +0000

    Fixed typo.

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/morhetz/gruvbox.git ~/donwload/gruvbox
+git clone https://github.com/morhetz/gruvbox.git ~/download/gruvbox
 cp -r ~/download/gruvbox/colors ~/.vim/
 cp .vimrc ~/
