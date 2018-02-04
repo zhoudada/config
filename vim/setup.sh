9ed023af1ae03d05153370c1c4f56ec464eeb2b6
zhoudada <zhoudada123@hotmail.com>
2018-02-04 12:06:36 +0000

    Added vim setup script.

---
+++ vim/setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/morhetz/gruvbox.git ~/donwload/gruvbox
+cp -r ~/download/gruvbox/colors ~/.vim/
+cp .vimrc ~/
