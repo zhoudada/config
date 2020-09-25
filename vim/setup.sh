ced6d60c03122ae0af2d0864586874d61bd09c46
Zhoudada <zhoudada123@hotmail.com>
2020-09-25 11:00:01 -0700

    Make shebang more portable.

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/morhetz/gruvbox.git ~/download/gruvbox
 cp -r ~/download/gruvbox/colors ~/.vim/
